f831ee6b09b53389f502e60250f2b2c81f72f1a9
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2017-09-05 11:09:02 -0300

    talvez esteja terminada

--- 7.s
+++ 7.s
@@ -1,8 +1,7 @@
 .data
-a:	.word 0
-n: 	.word 3
-s: 	.word 5
-sd:	 .word 0
+n: 	.word 0
+s: 	.word 
+sd:	.word 0
 	.text
 	.globl start
 	.ent start
@@ -11,69 +10,90 @@ start:
 
 	lw $8, n 					# $8 = n
 	lw $9, s					# $9 = s
-	lw $3, a					# start $3 = 0
+	add $3, $0, $0				# start $3 = 0
 	
-	slt $10, $8, $0				# $8 < 0 ?
-	bne $10, $0, ltz			# $10 != 0 -> $10 == 1 // ltz = lesser than zero
-	slt $10, $9, $0				# $9 < 0 ?
-	bne $10, $0, ltz			# $10 != 0 -> $10 == 1
+	slt $10, $8, $9 			# is s greater than n?
+	bne $10, $0, s_greater		# if so, go to s_greater
 	
-	beq $8, $9, equal	
+	slt $10, $8, $0				# is $8 lesser than zero?
+	bne $10, $0, ltz			# if it is go to ltz // ltz = lesser than zero
+	slt $10, $9, $0				# is $9 lesser than zero?
+	bne $10, $0, ltz			# if it is, go to ltz // ltz = lesser than zero
 	
-	beq $8, $0, z_equal
-	beq $9, $0, z_equal					
+	beq $8, $9, equal			# are $8 and $9 equal? if so, go to equal
 	
-	slt $10, $8, $9 			# s > n ?
-	beq $10, $0, factorial		
+	beq $8, $0, z_equal			# is $8 equal zero? if it is, go to z_equal
+	beq $9, $0, z_equal			# is $9 equal zero? if it is, go to z_equal
 	
+	j combination				# else do the combination
+	
+s_greater:
+
 	addi $3, $0, 4				# $3 = 4
-	j factorial 
+	j done
 	
 ltz:
 
 	addi $3, $0, 5				# $3 = 5
-	j factorial
+	j done
 
-	
 equal:
 
 	addi $3, $0, 6				# $3 = 6
-	j factorial
+	j done
 
 z_equal:
 
 	addi $3, $0, 7				# $3 = 7
-	j factorial
-	
-factorial: 						# $ fatorial de n
-	add $4, $0, $8
-	jal fact
 	j done
 	
+combination:
+
+	add $4, $0, $8				# parameter n to the fact
+	jal fact					# call fact function
+	add $11, $0, $2				# $11 stores !n
+	add $4, $0, $9				# parameter s to the fact
+	jal fact					# call fact function
+	add $12, $0, $2				# $12 stores !s
+	add $4, $0, $8				# $4 = $8
+	sub $4, $4, $9 				# $4 = $8 - $9 // (n - s)
+	jal fact					# call fact function
+	add $13, $0, $2				# $13 stores !(n - s)
+	mul $12, $12, $13			# $12 = !s * !(n - s)
+	div $11, $11, $12			# $11 = !n / (!s * !(n - s))
+	
+	sw $11, sd					# sd = !n / (!s * !(n - s))
+	
+	j done
+	
+done:
+	break
+	
+## @param: $4 as the value used in the factorial
+## @return: $2 as the fact result
+## @altered_reg: $10, $2
 fact:
 	
-	addi $29, $29, -8 			
+	addi $29, $29, -8 			# adjust stack for 2 items
 	sw $31, 4($29) 				# save the return address
  	sw $4, 0($29) 				# save the argument n
  	
-	slti $8,$4,1 				# (n < 1) ?
-	beq $8, $0, L1	 			# if n >= 1, goto L1
+	slti $10, $4, 1 			# is n lesser than 1?
+	beq $10, $0, L1	 			# if not, goto L1
 	
-	addi $2,$0,1	 			# else return 1
-	addi $29, $29, 8 			# pop 2 items off stack
-	jr $31 						# return to caller
+	addi $2, $0, 1	 			# else return 1
+	addi $29, $29, 8 			# pop 2 items
+	jr $31 						# return
 	
 L1: 
 
-	addi $4,$4,–1	 			# n >= 1: argument gets (n – 1)
- 	jal fact 					# call fact with (n –1)
- 	lw $4, 0($29) 				# return from jal: restore argument n
-	lw $31, 4($29) 				# restore the return address
-	addi $29, $29, 8 			# adjust stack pointer to pop 2 items
-	mul $2, $4, $2 				# return n * fact (n – 1)
-	jr $31 						# return to the caller
-	
-done:
-		break
+	addi $4, $4, -1	 			# n--
+ 	jal fact 					# call fact with n-1
+ 	
+ 	lw $4, 0($29) 				# $4 = n
+	lw $31, 4($29) 				# $31 receives the last return address
+	addi $29, $29, 8 			# pop 2 items
+	mul $2, $4, $2 				# $2 = n * fact (n – 1)
+	jr $31  					# return
 	
 .end start
